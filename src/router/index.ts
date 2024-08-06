932cfb5ba1623e04a71630f9f9850d9ad40622d6
zhengkunjin <zhengkunjin@126.com>
2024-08-06 11:38:57 +0800

    my fifth submit

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
  
 const router = createRouter({
 //   history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory('my-vue-app'),
+  history: createWebHistory('/my-vue-app/'),
   routes: [
     {
       path: '/',
